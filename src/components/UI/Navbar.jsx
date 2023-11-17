e5f6d144f5cdd87aaab99dd49bea1d53b2f1d04e
DShreyan <dshreyan25@google.com>
2023-11-17 15:57:02 +0530

    changed navbar and footer

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,30 +1,24 @@
 import React from 'react'
 import '../CSS/Navbar.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
-    return (
-      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-      <div className="container-fluid">
-        <Link className="navbar-brand logo" to="/"><span>V</span>ihari</Link>
-        <form className="d-flex mx-5" role="search">
-            {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-success" type="submit">Search</button> */}
-        <input name="tname" className="form-control me-2 search" type="search" placeholder="Search Tours" aria-label="Search"
-        id="header-search-bar" />
-      <button className="btn1" type="submit" style={{color: '#06bbcc',backgroundColor: 'transparent'}}>
-        <i className="fa fa-search"></i>
-      </button>
-          </form>
+  return (
+    <div class="navbar-header">
+      <Link className="navbar-brand logo" to="/"><span>V</span>ihari</Link>
+      <nav className="navbar navbar-expand-lg sticky-top p-0 burge">
+        <form className="d-flex mx-5 searchtext" role="search">
+          <input name="tname" className="form-control me-2 search" type="search" placeholder="Search Tours" aria-label="Search" id="header-search-bar" />
+          <button className="btn1" type="submit" style={{ color: '#06bbcc', backgroundColor: 'transparent' }}>
+            <i className="fa fa-search"></i>
+          </button>
+        </form>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <i className="fas fa-hamburger" style={{color: '#bbbbbb'}}></i>
+          <i className="fas fa-hamburger" style={{ color: '#bbbbbb' }}></i>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Link</Link>
+              <Link className="nav-link" aria-current="page" to="/">Home</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/">Destination</Link>
@@ -44,14 +38,14 @@ const Navbar = () => {
             <li className="nav-item">
               <Link className="nav-link" to="/">Contact</Link>
             </li>
-            <li className="nav-item">
-            <Link className="nav-link" to="/login">{!localStorage.token?<><i className="fa fa-user mx-2" aria-hidden="true" id="login-btn"></i>Login</>:<><i className="fa fa-sign-out mx-2" aria-hidden="true" id="login-btn"></i>Logout</>}</Link>
+            <li className="nav-item icon">
+              <Link className="nav-link" to="/login">{!localStorage.token ? <><i className="fa fa-user mx-2" aria-hidden="true" id="login-btn"></i></> : <><i className="fa fa-sign-out mx-2" aria-hidden="true" id="login-btn"></i>Logout</>}</Link>
             </li>
           </ul>
         </div>
-      </div>
-    </nav>
-    )
+      </nav>
+    </div>
+  )
 }
 
 export default Navbar
