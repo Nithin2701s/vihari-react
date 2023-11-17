7606772d8ed6e1bd08e9e72b369b106d70326415
Nithin2701s <115217815+Nithin2701s@users.noreply.github.com>
2023-11-17 17:58:09 +0530

    Navbar changes

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,34 +1,24 @@
 import React from 'react'
 import '../CSS/Navbar.css'
-import {Link, useNavigate} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
-   const navigate =useNavigate()
-    return (
-      <nav className="navbar  bg-dark navbar-dark">
-      <div className="container-fluid">
-        <Link className="navbar-brand logo" to="/"><span>V</span>ihari</Link>
-        <form className="d-flex mx-5" role="search">
-        <input name="tname" className="form-control me-2 search" type="search" placeholder="Search Tours" aria-label="Search"
-        id="header-search-bar" />
-      <button className="btn1" type="submit" style={{color: '#06bbcc',backgroundColor: 'transparent'}}>
-        <i className="fa fa-search"></i>
-      </button>
-          </form>
-            <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-            <i className="fas fa-hamburger" style={{color: '#bbbbbb'}}></i>
-    </button>
-        <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">VIHARI</h5>
-        <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-end  flex-grow-1 pe-3">
+  return (
+    <div class="navbar-header">
+      <Link className="navbar-brand logo" to="/"><span>V</span>ihari</Link>
+      <nav className="navbar navbar-expand-lg sticky-top p-0 burge">
+        <form className="d-flex mx-5 searchtext" role="search">
+          <input name="tname" className="form-control me-2 search" type="search" placeholder="Search Tours" aria-label="Search" id="header-search-bar" />
+          <button className="btn1" type="submit" style={{ color: '#06bbcc', backgroundColor: 'transparent' }}>
+            <i className="fa fa-search"></i>
+          </button>
+        </form>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <i className="fas fa-hamburger" style={{ color: '#bbbbbb' }}></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Link</Link>
+              <Link className="nav-link" aria-current="page" to="/">Home</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/">Destination</Link>
@@ -48,15 +38,14 @@ const Navbar = () => {
             <li className="nav-item">
               <Link className="nav-link" to="/">Contact</Link>
             </li>
-            <li className="nav-item">
-              {!localStorage.token?<Link className="nav-link" to="/login"><i className="fa fa-user mx-2" aria-hidden="true" id="login-btn"></i>Login</Link>:<Link className="nav-link" to="/" onClick={()=>{localStorage.removeItem('token'); navigate('/')}}><i className="fa fa-sign-out mx-2" aria-hidden="true" id="login-btn" ></i>Logout</Link>}
+            <li className="nav-item icon">
+              <Link className="nav-link" to="/login">{!localStorage.token ? <><i className="fa fa-user mx-2" aria-hidden="true" id="login-btn"></i></> : <><i className="fa fa-sign-out mx-2" aria-hidden="true" id="login-btn"></i>Logout</>}</Link>
             </li>
           </ul>
         </div>
-      </div>
-      </div>
-    </nav>
-    )
+      </nav>
+    </div>
+  )
 }
 
 export default Navbar
