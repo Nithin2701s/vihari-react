84b2d03e2a171fcf3eeeff1fcc2fcf10ff76d9d6
Srikar <srikar.a21@iiits.in>
2023-11-29 17:43:45 +0530

    admin db buses tours

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ import {
 } from 'react-router-dom'
 import UserState from './context/User/UserState';
 import Layout from './components/UI/Layout';
+import AddBus from './components/UI/AddBus';
+import AllBuses from './components/UI/AllBuses';
+import EditBus from './components/UI/EditBus';
+import AllUsers from './components/UI/AllUsers';
+import AddUser from './components/UI/AddUser';
 function App() {
   return (
     <div className="App">
@@ -18,6 +23,11 @@ function App() {
           <Route exact path='/' element={ <Home/>}/>
           <Route exact path='/login' element={ <Login/>}/>
           <Route exact path='/layout' element={ <Layout/>}/>
+          <Route exact path='/admindb/allbuses' element={<AllBuses/>} />
+          <Route exact path='/admindb/allusers' element={<AllUsers/>} />
+          <Route exact path='/admindb/addbus' element={<AddBus/>}/>
+          <Route exact path='/admindb/adduser' element={<AddUser/>}/>
+          <Route exact path="/admindb/editbus/:id" element={ <EditBus/>}/>
         </Routes>
     </Router>
     </UserState>
